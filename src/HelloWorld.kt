aeb0b159fc635e2d0c207cf19cd26897d9418081
Kirill Kramarenko <iamrfx@gmail.com>
2024-08-02 13:21:50 +0200

    Worked with Collections

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -5,8 +5,9 @@ fun main(args: Array<String>) {
 //    stringExpressions()
 //    readNumberFromCommandLine()
 //    printTypes()
+//    printNullableAndNonnullVariables()
 
-    printNullableAndNonnullVariables()
+    workWithCollections()
 }
 
 fun helloWorld() {
@@ -59,4 +60,31 @@ fun printNullableAndNonnullVariables() {
 
     // !! / !!. - Non-null assertion, should be used only in tests
     println(dogName!!.length)
+}
+
+fun workWithCollections() {
+    val trafficLightColors = listOf("red", "yellow", "green")
+
+    val darkColors = mutableListOf<String>()
+    darkColors.add("black")
+    darkColors.add("grey")
+    darkColors.add("brown")
+
+    println("The first dark color I remembered is ${darkColors[0]}")
+
+    val set1 = setOf(1, 2, 5, 7, 9)
+    val set2 = setOf(2, 3, 4, 7)
+
+    println("set1 = $set1")
+    println("set2 = $set2")
+    println("Intersection of set1 and set2 is ${set1.intersect(set2)}")
+    println("Union of set1 and set2 is ${set1.union(set2).sorted()}")
+
+    val ageByName = mapOf<String, Int>(
+        "Ivan" to 19,
+        "Jack" to 21,
+        "John" to 35
+    )
+
+    println(ageByName)
 }
